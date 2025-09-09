ab9925c2d6643065e75ab79d6e880e79b2bac9c6
Agathanicolle <202511493@urp.edu.pe>
2025-09-09 19:43:16 +0000

    ejercicio 1 y 2

--- empleado_tiempo_completo.py/ejercicio_02.py
+++ empleado_tiempo_completo.py/ejercicio_02.py
@@ -1,9 +1,7 @@
-"""
-Programa: ejercicio_02.py
-Fecha: 09-09-25
-"""
-class vehículo:
-    def __init__(self,marca: str, modelo: str)
+import unittest
+
+class Vehiculos:
+    def __init__(self,marca: str, modelo: str):
         self.marca = marca
         self.modelo = modelo
         
@@ -16,9 +14,8 @@ class Automovíl(vehículo):
         self.num_partes = num_partes
         
         def es_deportivo(self) -> bool:
-             return(self.num.puertas ==2)              
-                   
-                   
+            return(self.num.puertas ==2)              
+                             
   #Prueba Unitaria                 
 class TestPruebaAutomovíl(whittest,TestCase):
     def setUp(self):
@@ -30,7 +27,18 @@ class TestPruebaAutomovíl(whittest,TestCase):
     def test_auto_deportivo(self):
         self.assertTrue(self.auto2.es_deportivo())
         
+    def test_auto_deportivo_2(self):
+        self.assertEqual(self.auto2.es_deportivo())
+        
+    def test_auto_deportivo_3(self):
+        self.assertLess(self.auto2.num_puertas,3)
+        
         
 if __name__ == '__main__':
-    unittest.main()
-    
+    #unittest.main()
+    #Prueba de la clase automovíl
+    auto3 = Automovíl('Honda, Accord', 2)
+    if auto3.es_deportivo():
+       print (auto3.mostrar_info() + '\n (Auto deportivo)')
+    else:
+       print (auto3.mostar_info () + '\n (auto sedán)')
